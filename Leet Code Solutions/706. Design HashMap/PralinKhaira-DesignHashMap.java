a4d080ea96b300f33a5e05736761f4411b4f88db
BitH0xer <be.coder.19@gmail.com>
2023-10-04 20:29:59 +0530

    Update PralinKhaira-DesignHashMap.java

--- Leet Code Solutions/706. Design HashMap/PralinKhaira-DesignHashMap.java
+++ Leet Code Solutions/706. Design HashMap/PralinKhaira-DesignHashMap.java
@@ -1,1 +1,20 @@
+/*
+Problem Link: https://leetcode.com/problems/design-hashmap/
+*/
 
+class MyHashMap {
+    int[] data;
+    public MyHashMap() {
+        data = new int[1000001];
+        Arrays.fill(data, -1);
+    }
+    public void put(int key, int val) {
+        data[key] = val;
+    }
+    public int get(int key) {
+        return data[key];
+    }
+    public void remove(int key) {
+        data[key] = -1;
+    }
+}
