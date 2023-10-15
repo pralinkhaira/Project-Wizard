b89505056fa4d06a05001108401335804e1a3e02
BitH0xer <be.coder.19@gmail.com>
2023-10-15 15:13:10 +0530

    Create Create PralinKhaira-SamePlace.cs

---
+++ Leet Code Solutions/1269. Number of Ways to Stay in the Same Place After Some Steps/Create PralinKhaira-SamePlace.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int NumWays(int steps, int arrLen) {
+        int m = steps;
+        int n = Math.Min(steps / 2 + 1, arrLen);
+        
+        int[,] dp = new int[m + 1, n];
+        dp[0, 0] = 1;
+        
+        int mod = 1000000007;
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i, j] = dp[i - 1, j];
+                if (j > 0) {
+                    dp[i, j] = (dp[i, j] + dp[i - 1, j - 1]) % mod;
+                }
+                if (j < n - 1) {
+                    dp[i, j] = (dp[i, j] + dp[i - 1, j + 1]) % mod;
+                }
+            }
+        }
+        
+        return dp[m, 0];
+    }
+}
