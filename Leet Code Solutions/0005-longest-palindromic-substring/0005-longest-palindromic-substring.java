e307e935a430b246d78ee83481eadd3d1e871140
BitH0xer <be.coder.19@gmail.com>
2023-10-27 13:08:46 +0530

    POTD Added LeetCode

---
+++ Leet Code Solutions/0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+    
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
