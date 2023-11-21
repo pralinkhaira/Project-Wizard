f25b2ad2fdbf94bce7cedc28accbdcc9eaa3590e
BitH0xer <be.coder.19@gmail.com>
2023-11-21 19:52:45 +0530

    potd

---
+++ Geeks For Geeks Solutions/Determine if Two Trees are Identical/README.md
@@ -0,0 +1,18 @@
+<h2><a href="https://www.geeksforgeeks.org/problems/determine-if-two-trees-are-identical/1">Determine if Two Trees are Identical</a></h2><h3>Difficulty Level : Easy</h3><hr><div class="problems_problem_content__Xm_eO"><p><span style="font-size: 14pt;">Given two binary trees, the task is to find if both of them are identical or not.<br><strong>Note:</strong> You need to return true or false, the printing is done by the driver code.</span></p>
+<p><span style="font-size: 14pt;"><strong>Example 1:</strong></span></p>
+<pre><span style="font-size: 14pt;"><strong>Input:
+</strong>     1          1
+&nbsp;  /   \      /   \
+&nbsp; 2     3    2     3
+<strong>Output: <br></strong>Yes<strong>
+Explanation: <br></strong>There are two trees both having 3 nodes and 2 edges, both trees are identical having the root as 1, left child of 1 is 2 and right child of 1 is 3.</span></pre>
+<p><span style="font-size: 14pt;"><strong>Example 2:</strong></span></p>
+<pre><span style="font-size: 14pt;"><strong>Input:
+</strong>    1       1
+&nbsp; /  \     /  \
+&nbsp;2    3   3    2
+<strong>Output: <br></strong>No<strong>
+Explanation: </strong>There are two trees both having 3 nodes and 2 edges, but both trees are not identical.</span></pre>
+<p><span style="font-size: 14pt;"><strong>Your task:</strong><br>Since this is a functional problem you don't have to worry about input, you just have to complete the function <strong>isIdentical()</strong> that takes two roots as parameters and returns true or false. The printing is done by the driver code.</span></p>
+<p><span style="font-size: 14pt;"><strong>Expected Time Complexity: </strong>O(N).<br><strong>Expected Auxiliary Space:&nbsp;</strong>O(Height of the Tree).</span></p>
+<p><span style="font-size: 14pt;"><strong>Constraints:</strong><br>1 &lt;= Number of nodes &lt;= 10<sup>5</sup><br>1 &lt;=Data of a node &lt;= 10<sup>9</sup></span></p></div><p><span style=font-size:18px><strong>Company Tags : </strong><br><code>Flipkart</code>&nbsp;<code>Amazon</code>&nbsp;<code>Microsoft</code>&nbsp;<br><p><span style=font-size:18px><strong>Topic Tags : </strong><br><code>Tree</code>&nbsp;<code>Data Structures</code>&nbsp;
---
+++ Geeks For Geeks Solutions/Determine if Two Trees are Identical/determine-if-two-trees-are-identical.java
@@ -0,0 +1,163 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Contributed by Sudarshan Sharma
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t-- > 0){
+	            String s1 = br.readLine();
+    	    	String s2 = br.readLine();
+    	    	Node root1 = buildTree(s1);
+    	    	Node root2 = buildTree(s2);
+                Solution g = new Solution();
+            //System.out.println(g.isIdentical(root,roott));
+			    boolean b = g.isIdentical(root1,root2);
+			    if(b==true)
+				    System.out.println("Yes");
+			    else
+				    System.out.println("No");
+	        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+/*
+
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){
+        data=d;
+        left=right=null;
+    }
+}*/
+
+class Solution
+{
+    //Function to check if two trees are identical.
+    boolean isIdentical(Node root1, Node root2)
+    {
+        // Code Here
+         if (root1 == null && root2 == null) 
+         {
+
+//if both the trees are null
+            return true;
+        } 
+        else if (root1 == null || root2 == null) 
+        {
+
+//If either of the tree is null
+            return false;
+        }
+        else if (root1.data != root2.data)
+        {
+
+//if the data field is not equal
+            return false;
+        } 
+        else 
+        {
+
+//Recursive call to verify left and right sub Trees
+            return isIdentical(root1.left, root2.left) && isIdentical(root1.right, root2.right);
+        }
+        
+    }
+    
+}
